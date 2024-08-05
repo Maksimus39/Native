a7928f5c1dc4f0675d6a0d9b800c564a1611c2df
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-05 17:49:16 +0300

    Преобразование тиров

---
+++ learnJS-самоучитель языка программирования/Основы JavaScript/преобразование типов/main.js
@@ -0,0 +1,22 @@
+// Строковое преобразование
+
+let value = true;
+alert(typeof  value)
+
+value = String(value);
+alert(typeof  value)
+
+// Численное преобразование
+alert('40' / '20')
+
+let str = '123';
+alert(typeof  str);
+
+let num = Number(str)
+alert(typeof  num)
+
+// Явное преобразование часто применяется, когда мы ожидаем получить число из строкового контекста, например из текстовых полей форм.
+
+// Логическое преобразование
+
+alert(Boolean(1))

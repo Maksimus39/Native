791edf72fea9b8eded8ad2042b427f188fd6a42a
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-05 17:06:00 +0300

    Переменные

---
+++ learnJS-самоучитель языка программирования/Основы JavaScript/переменные/main.js
@@ -0,0 +1,39 @@
+let message;
+
+message = 'Hello World';
+
+console.log(message)
+
+let user = 'John', age = 25, message2 = 'Hello World';
+
+console.log(user, age, message2);
+
+let user2 = "Ben", age2 = 30, message3 = "Hello World";
+console.log(user2, age2, message3);
+
+let user3 = "Max"
+    , age3 = 40
+    , message4 = "Hi"
+console.log(user3, age3, message4);
+
+// const
+
+const myBirthDay = "05.08.2024"
+console.log(myBirthDay)
+
+// практика
+
+let admin;
+let name;
+
+name = "Джон"
+admin = name;
+console.log(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "Max";
+
+
+const BIRTHDAY = '18.04.1982'; // использовать заглавные буквы?
+
+const AGE = someCode(BIRTHDAY); // а здесь?

5719f79e59c382b5cc8eab7520115b413aafb227
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-05 17:37:27 +0300

    Взаимодействие: alert, prompt, confirm

---
+++ learnJS-самоучитель языка программирования/Основы JavaScript/Взаимодействие alert, prompt, confirm/main.js
@@ -0,0 +1,16 @@
+// alert
+alert('Hello World!');
+
+
+// prompt
+let age = prompt('Сколько тебе лет?', 41)
+alert(`Тебе ${age} лет `)
+
+// confirm
+let isBoss = confirm('Ты здесь главный')
+alert(isBoss)
+
+// задачи
+
+let currentUserName = prompt('Как тебя зовут')
+alert(`Твоё имя ${currentUserName}!`)

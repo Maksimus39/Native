a31bf09075f7620cc149a6fe9c98f0783ba947b2
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-31 14:48:04 +0300

    Изучил метод массива splice

---
+++ learnJS-самоучитель языка программирования/array/main.js
@@ -0,0 +1,30 @@
+const arr = [1, 2, 3, 4, 5];
+arr.push(6)
+console.log(arr)
+
+
+// splice ---------------------  arr.splice(start[, deleteCount, elem1, ..., elemN])
+
+arr.splice(0, 4)
+console.log(arr)
+
+const strArr = ["Я", "изучаю", "JavaScript"];
+strArr.splice(2, 1)
+console.log(strArr)
+
+const stringArray = [
+    "Привет, мир!",
+    "JavaScript",
+    "Массив строк",
+    "Открытие нового",
+    "Кодирование",
+    "Программирование",
+    "Технологии",
+    "Инновации",
+    "Разработка",
+    "Обучение"
+];
+
+stringArray.splice(5,5)
+console.log(stringArray)
+

fc221f00905ee1f2f9a1e3a914a80f8f9a751253
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-30 22:14:20 +0300

    basic functions

---
+++ learnJS-самоучитель языка программирования/function/main.js
@@ -0,0 +1,56 @@
+// Обязателен ли "else"?
+
+// function checkAge(age) {
+//     if (age > 18) {
+//         return true;
+//     } else {
+//         // ...
+//         return confirm('Родители разрешили?');
+//     }
+//
+// }
+
+//console.log(checkAge(10))
+
+// Перепишите функцию, используя оператор '?' или '||'
+
+// function checkAge2(age) {
+//     if (age > 18) {
+//         return true;
+//     } else {
+//         return confirm('Родители разрешили?');
+//     }
+// }
+
+// function checkAge(age) {
+//     return (age > 18) ? true : confirm('Родители разрешили?');
+// }
+
+// function checkAge(age) {
+//     return (age > 18) || confirm('Родители разрешили?');
+// }
+
+
+// Функция min(a, b)
+// function min(a, b) {
+//     if (a < b) {
+//         return a
+//     } else {
+//         return b
+//     }
+//
+// }
+//
+// console.log(min(2, 5))
+// console.log(min(3, -1))
+// console.log(min(1, 1))
+
+
+// Функция pow(x,n)
+// function pow(x, n) {
+//     return x ** n
+// }
+//
+// console.log(pow(3, 2))
+// console.log(pow(3, 3))
+// console.log(pow(1, 100))

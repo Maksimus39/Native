6ae450efa10a074ada04f4278e1831503b4dfd47
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-10-02 09:58:55 +0300

    lesson 4

--- lessonJsNativeIncubator/Monday/lesson 04/main4.js
+++ lessonJsNativeIncubator/Monday/lesson 04/main4.js
@@ -74,26 +74,174 @@
 
 // ------------------------------------ Практическое применение ------------------------------
 
-const STATUS_BUSY = 'busy';
-const STATUS_READY = 'ready';
+// const STATUS_BUSY = 'busy';
+// const STATUS_READY = 'ready';
+//
+// const STATUS_LABELS = {
+//     [STATUS_BUSY]: 'ожидает',
+//     [STATUS_READY]: 'готово'
+// }
+//
+// const drivers = [
+//     {name: 'Pavel', status: 'busy'},
+//     {name: 'Anna', status: 'ready'},
+//     {name: 'Kiryll', status: 'busy'}
+// ]
+//
+// const driversStatuses = drivers.map(driver => {
+//     const {status} = driver;
+//     return STATUS_LABELS[status]
+// })
+//
+// console.log(driversStatuses) // [ 'ожидает', 'готово', 'ожидает' ]
 
-const STATUS_LABELS = {
-    [STATUS_BUSY]: 'ожидает',
-    [STATUS_READY]: 'готово'
-}
 
-const drivers = [
-    {name: 'Pavel', status: 'busy'},
-    {name: 'Anna', status: 'ready'},
-    {name: 'Kiryll', status: 'busy'}
-]
+// object.keys() - возвращает массив ключей  ------------------------------------------
 
-const driversStatuses = drivers.map(driver => {
-    const {status} = driver;
-    return STATUS_LABELS[status]
-})
+// const drivers =  {name: 'Pavel', status: 'busy'}
+//
+// const result = Object.keys(drivers)
+// console.log(result) //  [ 'name', 'status' ]
 
-console.log(driversStatuses) // [ 'ожидает', 'готово', 'ожидает' ]
+// !!!!!
+
+// const drivers = {name: 'Pavel', status: 'busy', coutry: {id: 1}}
+//
+// const result1 = Object.keys(drivers)
+// console.log(result1)
+
+
+// Object values() - возвращает массив значений -----------------------------------------
+
+// const drivers =  {name: 'Pavel', status: 'busy'}
+//
+// const result = Object.values(drivers)
+// console.log(result)  // [ 'Pavel', 'busy' ]
+
+// !!!!
+
+// const drivers = {name: 'Pavel', status: 'busy', coutry: {id: 1}}
+//
+// const result = Object.values(drivers)
+// console.log(result)  // [ 'Pavel', 'busy', { id: 1 } ]
+
+
+//Object.entries() - возвращает массив пар [ключ , значение ] -----------------------------------
+
+// const drivers =  {name: 'Pavel', status: 'busy'}
+//
+// const result = Object.entries(drivers)
+// console.log(result)  // [ [ 'name', 'Pavel' ], [ 'status''busy' ] ]
+
+// !!!!!!!!
+
+// const drivers = {name: 'Pavel', status: 'busy', coutry: {id: 1}}
+//
+//  const result = Object.entries(drivers)
+//
+// console.log(result) // [ [ 'name', 'Pavel' ], [ 'status', 'busy' ], [ 'coutry', { id: 1 } ] ]
+
+// --------------------------------------------------------------------------------------------------
+// new Map, new set
+
+// new Map () - это коллекция ключ/значение
+
+//let map = new Map();
+
+// console.log('map : ',map)  // map :  Map(0) {}
+
+// map.set('1', 'first value number')
+// map.set(1, 'number')
+// map.set(true, 'boolean value')
+// map.set([], 'array')
+// map.set({}, 'object')
+
+// console.log(map)
+
+// Map(5) {
+//     '1' => 'first value number',
+//         1 => 'number',
+//         true => 'boolean value',
+//         [] => 'array',
+//         {} => 'object'
+// }
+
+
+// --------------------------------------------------------------------------------------------------------
+
+// let map = new Map();
+//
+// map.set('1', 'first value number')
+// map.set(1, 'number')
+// map.set(true, 'boolean value')
+// map.set([], 'array')
+// map.set({}, 'object')
+//
+// console.log(map.get('1'))    // first value number
+// console.log(map.get(true))  // boolean value
+//
+// map.clear()
+// //console.log(map)  // Map(0) {}
+//
+//
+// map.set('1','one')
+// map.set('2','two')
+//
+// map.delete('2')
+//
+// console.log(map)           // Map(1) { '1' => 'one' }
+// console.log(map.size)      // 1
+
+
+// --------------------------------------------------------------------------------------------------------
+
+// new Set() - особый вид коллекции ( множество без ключей). Все значения уникальные
+
+// let set = new Set();
+//
+// console.log(set)            // Set(0) {}
+//
+// set.add('value1')
+// set.add('value2')
+// set.add('value1')
+//
+// //console.log('set :', set)   // set : Set(2) { 'value1', 'value2' }
+//
+// set.delete('value1')
+// console.log('set after deleting:', set)      // set after deleting: Set(1) { 'value2' }
+//
+//
+// set.has('value1')
+// console.log('is it here?:', set.has('value1'))   // is it here?: false
+// console.log('is it here?:', set.has('value2'))   // is it here?: true
+//
+//
+// set.clear()
+// console.log('set:',set)                               // set: Set(0) {}
+
+
+// ---------------------------------------------------------------------------------------------------
+
+// let set = new Set(['value1', 'value2', 3, 4]);
+// console.log(set)   // Set(4) { 'value1', 'value2', 3, 4 }
+//
+// // ------------------------------------
+
+let numbers = [1, 1, 1, 3, 4, 5, 6, 76, 7, 8, 9, 0, 6]
+
+// let uniqueNumbers = new Set(numbers);
+// console.log(uniqueNumbers)  // Set(10) { 1, 3, 4, 5, 6, 76, 7, 8, 9, 0 }
+
+// let newArr = [...uniqueNumbers]
+// console.log(newArr)         // [1, 3, 4, 5, 6, 76, 7, 8, 9, 0]
+
+// or
+let result = [...new Set(numbers)]
+console.log(result)    // // [1, 3, 4, 5, 6, 76, 7, 8, 9, 0]
+
+
+
+
 
 
 

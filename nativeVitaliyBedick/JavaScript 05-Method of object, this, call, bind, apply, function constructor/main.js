815342dc5ab1ef4d71be9abb4fc1fc31fe41f48a
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-11-27 09:17:15 +0300

    JavaScript. 05-Method of object, this, call, bind, apply, function constructor - finish lesson

--- nativeVitaliyBedick/JavaScript 05-Method of object, this, call, bind, apply, function constructor/main.js
+++ nativeVitaliyBedick/JavaScript 05-Method of object, this, call, bind, apply, function constructor/main.js
@@ -4,34 +4,34 @@
 
 // способы создания метода объекта
 
-const user = {
-    name: 'Max',
-    age: 41,
-    // 1 способ
-    sayHi() {
-        return 'Hello World!'
-    },
-    // 2 способ
-    sayHi2: function () {
-        return `Hello ${this.name}!`
-    },
-    getAge: function (inc1) {
-        return `Мне ${this.age + inc1} год !`
-    },
-    getFullInfo() {
-        return `Имя ${this.name}, Возраст ${this.age}`
-    }
-}
-// 3 способ
-user.sayHi3 = function () {
-    return 'Hello world-3'
-}
-
-
-const son = {
-    name: 'Andrey',
-    age: 11
-}
+// const user = {
+//     name: 'Max',
+//     age: 41,
+//     // 1 способ
+//     sayHi() {
+//         return 'Hello World!'
+//     },
+//     // 2 способ
+//     sayHi2: function () {
+//         return `Hello ${this.name}!`
+//     },
+//     getAge: function (inc1) {
+//         return `Мне ${this.age + inc1} год !`
+//     },
+//     getFullInfo() {
+//         return `Имя ${this.name}, Возраст ${this.age}`
+//     }
+// }
+// // 3 способ
+// user.sayHi3 = function () {
+//     return 'Hello world-3'
+// }
+//
+//
+// const son = {
+//     name: 'Andrey',
+//     age: 11
+// }
 
 
 //=============================this==================================
@@ -150,17 +150,87 @@ const son = {
 // console.log(dev.getProgrammingLanguage())
 
 
+// const cat = {
+//     type: 'britich',
+//     method1: () => this.type,
+//     method2: function () {
+//        const fn = () = this.type
+//         return fn()
+//     }
+// }
+//
+// console.log(cat.method2())
+//
+//
+// const arrrow1 = () => {
+//     return this.type
+// }
+//
+//
+// const arrow2 = function () {
+//     return this.type
+// }
+//
+//
+// console.log(arrrow1.call(cat))
+// console.log(arrow2.call(cat))
+
+
 //=============================Функция-конструктор==================================
 
 // Функция-конструктор
 
-// для чего нужна
+// const person = {
+//     name: 'Denis',
+//     age: 24,
+//     isMan: true,
+// }
+
+
+// для чего нужна - для генерации новых объектов
 
 // Правила при работе с функцией конструктор
 
+// 1 Имя функции начинается с большой буквы
+// 2 При её использовании использовать ключевое слово new
+
+
 // Базовая запись
 
+// function Person(name, age, isMan,salary) {
+//     this.name = name
+//     this.age = age
+//     this.isMan = isMan
+//     this.salary = salary
+// }
+//
+// const person1 = new Person('Maxim',41,true, 500)
+// const person2 = new Person('Larisa',37,true, 500)
+// const person3 = new Person('Andrey',11,true, 500)
+// const person4 = new Person('Bogdan',9,true, 500)
+//
+//
+// console.log(person1)
+// console.log(person2)
+// console.log(person3)
+// console.log(person4)
+
+
 // Разница по сравнению с обычной функцией (что возвращают)
+
+// function car() {
+//     return 123
+// }
+//
+// function Car() {
+//     return 123
+// }
+//
+// const car1 =car()
+// const car2 =new Car()
+//
+// console.log(car1)
+// console.log(car2)
 
 // Что делать, если нужно добавить свойство?
 
@@ -185,3 +255,63 @@ const son = {
 Создайте другой объект user, у которого есть свойства a и b.
 Используйте метод call или apply для вызова методов объекта calculator с аргументами из объекта user.
  */
+
+// const car = {
+//     brand: 'Audi',
+//     getBrand: function () {
+//         return this.brand
+//     }
+// }
+//
+// console.log(car.getBrand())
+//
+// const counter = {
+//     count: 10,
+//     increment: function () {
+//         return this.count + 1
+//     },
+//     decrement: function () {
+//         return this.count - 5
+//     },
+//     reset: function () {
+//         return this.count = 0
+//     }
+// }
+// console.log(counter.increment())
+// console.log(counter.decrement())
+// console.log(counter.reset())
+
+
+// const person = {
+//     name: 'Max',
+//     age: 42
+// }
+//
+//
+// function greet () {
+//     return `Меня зовут ${this.name}, и мне ${this.age} года!`
+// }
+//
+// const personGreat = greet.bind(person)
+//
+// console.log(personGreat())
+
+
+const calculator = {
+    x: 10,
+    y: 20,
+    sum() {
+        return this.x + this.y
+    },
+    mult() {
+        return this.x * this.y
+    }
+}
+
+const man ={
+    x:5,
+    y:8
+}
+
+console.log(calculator.sum.call(man))
+console.log(calculator.mult.call(man))

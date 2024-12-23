f5f786b9f3daa76dd2184eebda65ffe3b16d0265
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-12-23 09:19:55 +0300

    Lesson 10 - Занятие по JS This, .call .apply .bind, new - finich

--- lessonJsNativeIncubator/Thursday/lesson 4 - 2/main.js
+++ lessonJsNativeIncubator/Thursday/lesson 4 - 2/main.js
@@ -92,36 +92,107 @@
 // --------------------------------
 // 3
 
-const car = {
-    speed: 200,
-    showMaxSpeed(a, b) {
-        console.log(this.speed)
+// const car = {
+//     speed: 200,
+//     showMaxSpeed(a, b) {
+//         console.log(this.speed)
+//     }
+// }
+//
+// const scooter = {
+//     speed: 60
+// }
+//
+// // // call
+// // car.showMaxSpeed.call(scooter, 10, 20)
+// //
+// // // apply
+// // car.showMaxSpeed.apply(scooter, [20, 10])
+// //
+// // // bind
+// // car.showMaxSpeed.bind(scooter, 20, 10)()
+// // const foo = car.showMaxSpeed.bind(scooter, 20, 10)
+// // foo()
+//
+// setTimeout(() => car.showMaxSpeed(), 2000)
+// setTimeout( car.showMaxSpeed.bind(car), 2000)
+
+
+// ----------------------------------------------
+
+// const car = {
+//     speed: 200,
+//     showMaxSpeed(a, b) {
+//         console.log(this.speed)
+//     }
+// }
+//
+// const scooter = {
+//     speed: 60,
+// }
+//
+// const ferrari = {
+//     speed: 350
+// }
+//
+// car.showMaxSpeed.bind(scooter)() // 60
+
+
+//--------------------------------------------------
+
+// function Car(speed, brand) {
+//     // {}
+//     this.speed = speed  // {speed: 200}
+//     this.brand = brand  // {speed: 200, brand: 'bmw'}
+//     // return {speed: 200, brand: 'bmw'}
+// }
+//
+// const bmw = new Car(200, 'bmw')
+// const kia = new Car(200, 'kia')
+//
+// console.log(bmw)
+// console.log(kia)
+//
+//
+// // ---------------------------
+//
+// function foo() {}
+//
+// console.log(new foo())
+
+
+// -----------------------------------------------
+
+function foo() {
+    // this -> window
+    const x = 10
+
+    return {
+        x: 20,
+        bar: () => {
+            console.log(this.x)
+        },
+        baz: function () {
+            console.log(this.x)
+        }
     }
 }
 
-const scooter = {
-    speed: 60
-}
 
-// // call
-// car.showMaxSpeed.call(scooter, 10, 20)
-//
-// // apply
-// car.showMaxSpeed.apply(scooter, [20, 10])
-//
-// // bind 
-// car.showMaxSpeed.bind(scooter, 20, 10)()
-// const foo = car.showMaxSpeed.bind(scooter, 20, 10)
-// foo()
-
-setTimeout(()=>car.showMaxSpeed(),2000)
+// const obj1 = foo()
+// obj1.bar()
+// obj1.baz()
 
 
+const obj2 = foo.call({x: 30})
 
+// obj2.bar()  // 30
+// obj2.baz()  // 20
 
+let y = obj2.bar
+let z = obj2.baz
 
-
-
-
+y() // 30
+z() // undefined
 
 
